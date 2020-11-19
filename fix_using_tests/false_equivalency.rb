346b250067aab1e212fedbfc14a568af4588da49
Michael Cardona <mcardona9015@gmail.com>
2020-11-19 21:01:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -3,14 +3,12 @@ require 'pry'
 
 def get_user_input
   gets.chomp
-  binding.pry
 end
 
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
-  binding.pry
 end
 binding.pry
 def selection(num)
